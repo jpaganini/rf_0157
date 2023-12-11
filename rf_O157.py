cf5843a901f59b96acf9820475b7a1864ba0ded5
jpaganini <paganinijulian@gmail.com>
2023-12-11 17:10:47 +0100

    "Added code to create a CV scheme considering the population structure"

--- rf_O157.py
+++ rf_O157.py
@@ -529,8 +529,8 @@ def cross_model_balanced(model_input, label_df):
 
         model = RandomForestClassifier(n_estimators=200, 
                                    random_state=RSEED, 
-                                   max_features = 'sqrt',
-                                   n_jobs=-1, verbose = 1, max_depth=460)
+                                   max_features = 'log2',
+                                   n_jobs=-1, verbose = 1, max_depth=260)
         
         model.fit(features_resampled, labels_resampled)
         #test the model on test data
@@ -610,6 +610,134 @@ def cross_model_balanced(model_input, label_df):
 
     return final_res, final_imp, list_test_pred, list_test_labels
 
+
+def cross_model_balanced_grouped(model_input, label_df):
+    # columns = model_input.columns.tolist()
+    # ßprint (columns)
+    # columns = columns.remove('SYMP')
+
+    # Create 10-folds for cross validation
+    #row_nums = list(range(len(model_input)))
+    #random.shuffle(row_nums)
+    #splits = np.array_split(row_nums, 10)
+    #splits_indeces = []
+    #for array in splits:
+    #    splits_indeces += [model_input.index[array]]
+
+    #1. Import the data
+    all_labels = np.array(model_input['SYMP'])
+    features= model_input.drop(['SYMP','LINEAGE'], axis = 1)
+    lineages=np.array(model_input['LINEAGE'])
+
+    #set up the random seed
+    RSEED = 50
+
+    #Create an iterator to separate files in groups
+    logo = LeaveOneGroupOut()
+    cv_iterator = list(logo.split(features, all_labels, groups=lineages))
+
+    #to hold the max accuracy
+    acc_muvr_max = []
+
+    # data frame for storing probabilities of classification
+    final_res = pd.DataFrame()
+    # data frame for storing feature importance
+    final_imp = pd.DataFrame()
+    list_shap_values = list()
+    list_test_sets = pd.DataFrame()
+
+    # Test-set true lables
+    list_test_labels = []
+    # Test-set predictions
+    list_test_pred = []
+
+    for x in range(len(cv_iterator)):
+        test = model_input.iloc[cv_iterator[x][1]]
+        samples = test.index.values.tolist()
+        test_features = test.drop(['SYMP', 'LINEAGE'], axis=1)
+
+        test_labels = test['SYMP'].values.ravel()
+        train = model_input.iloc[cv_iterator[x][0]]
+        train_features = train.drop(['SYMP', 'LINEAGE'], axis=1)
+        train_labels = train['SYMP'].values.ravel()
+
+        features_resampled, labels_resampled = RandomOverSampler(random_state=RSEED).fit_resample(train_features,
+                                                                                                  train_labels)
+
+        model = RandomForestClassifier(n_estimators=200,
+                                       random_state=RSEED,
+                                       max_features='log2',
+                                       n_jobs=-1, verbose=1, max_depth=420)
+
+        model.fit(features_resampled, labels_resampled)
+        # test the model on test data
+
+        test_model = model.predict(test_features)
+        test_rf_probs = model.predict_proba(test_features)
+
+        list_test_labels = list_test_labels + list(test_labels)
+        list_test_pred = list_test_pred + list(test_model)
+
+        # 4. extract feature importance
+        feature_model = pd.DataFrame({'feature': test_features.columns,
+                                      x: model.feature_importances_}). \
+            sort_values(x, ascending=False)
+        feature_model = feature_model.set_index('feature')
+
+        final_imp = pd.concat([final_imp, feature_model], axis=1)
+
+        res_df = pd.DataFrame(
+            {'samples': samples, 'labels': test_labels, 'predictions': test_model, 'probabilties': zip(test_rf_probs)})
+
+        final_res = pd.concat([final_res, res_df])
+
+        # 5. get classification report
+        report_ = classification_report(
+            digits=6,
+            y_true=test_labels,
+            y_pred=test_model)
+
+        explainer = shap.TreeExplainer(model)
+        shap_values = explainer.shap_values(test_features)
+
+        list_shap_values.append(shap_values)
+        list_test_sets = pd.concat([list_test_sets, test_features])
+
+        acc_muvr_max += [model.score(test_features, test_labels)]
+    print(sum(acc_muvr_max) / len(acc_muvr_max))
+
+    # outputs commented out
+    final_res.to_csv(r'final_pred_grouped_test.tsv', sep='\t')
+    final_imp['average'] = final_imp.mean(numeric_only=True, axis=1)
+    final_imp = final_imp.sort_values('average', ascending=False)
+    final_imp.to_csv(r'final_grouped_test.tsv', sep='\t')
+
+    #   combining results from all iterations
+    shap_values = np.array(list_shap_values[0])
+    for i in range(1, len(list_shap_values)):
+        shap_values = np.concatenate((shap_values, np.array(list_shap_values[i])), axis=1)
+
+    # print shap uncomment
+    label_dict = dict(zip(label_df['Sequence'], label_df['ANN']))
+    fest = list_test_sets.columns.values.tolist()
+
+    anns = []
+    for f in fest:
+        anns.append(label_dict[f])
+
+    print(model.classes_)
+
+    # shap.summary_plot(shap_values[0], list_test_sets, feature_names=anns)
+    # shap.summary_plot(shap_values[1], list_test_sets,feature_names=anns)
+    shap.summary_plot(shap_values[2], list_test_sets, feature_names=anns)
+
+    # shap.summary_plot(shap_values[0], list_test_sets)
+    # shap.summary_plot(shap_values[1], list_test_sets)
+    # shap.summary_plot(shap_values[2], list_test_sets)
+
+    return final_res, final_imp, list_test_pred, list_test_labels
+
+
 def create_fasta(final_imp):
     features =  final_imp.index.values.tolist()
     #print (features)
@@ -678,10 +806,12 @@ label_df = load_feat_ann(ann_file)
 
 
 #print ("cv rf")
-final_res, final_imp, preds, labels = cross_model_balanced(feature_df, label_df)
-
-
+#final_res, final_imp, preds, labels = cross_model_balanced(feature_df, label_df)
 #calc_accuracy(preds, labels)
+
+final_res_grouped, final_imp_grouped, preds_grouped, labels_grouped = cross_model_balanced_grouped(feature_df, label_df)
+calc_accuracy(preds_grouped, labels_grouped)
+
 
 #imp_fasta = create_fasta(final_imp)
 
